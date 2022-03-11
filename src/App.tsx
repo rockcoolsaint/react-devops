02edc7a58dfbed8a8c6e9a8b5d744d3d421fca87
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2022-03-11 20:48:55 +0100

    add item to list

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 // import logo from './logo.svg';
 import ToDoList, { Item } from "./components/TodoList";
+import AddItem from "./components/AddItem";
 import './App.css';
 
 const initialList = [
@@ -18,18 +19,36 @@ const initialList = [
   },
 ];
 
+const isPartOf = (item: Item, items: Item[]): boolean => {
+  return items.some((it) => it.priority === item.priority);
+};
+
 class App extends React.Component<{}, { items: Item[] }> {
   constructor(props: any) {
     super(props);
     this.state = {
       items: initialList,
     };
+    this.addItem = this.addItem.bind(this);
+  }
+
+  addItem(item: Item) {
+    const { items } = this.state;
+
+    if (isPartOf(item, items)) {
+      alert(`Item with priorirty: ${item.priority} exists`);
+      return;
+    }
+    this.setState({
+      items: items.concat(item),
+    });
   }
 
   render() {
     const { items } = this.state;
     return (
       <div className="App">
+        <AddItem addItem={this.addItem} />
         <br />
         <ToDoList items={items} />
       </div>
